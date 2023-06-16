f4686143b29b5c05db79e28afd02ab62f29cbb67
antonm7 <migolkoanton@gmail.com>
2023-06-16 19:53:13 +0300

    styled button component

--- src/components/common/StyledButton/index.tsx
+++ src/components/common/StyledButton/index.tsx
@@ -1,12 +1,16 @@
 type Props = {
     title:string
     height:string
+    width?:string,
+    lgText?:boolean
 }
 
 export default function StyledButton ({
     title,
+    width,
+    lgText,
     height}:Props) {
     return (
-        <button className="bg-twitter-blue rounded-3xl px-5 w-min text-sm font-bold" style={{height}}>{title}</button>
+        <button className={`bg-twitter-blue rounded-3xl px-5 w-min ${lgText ? 'text-lg' : 'text-sm'} text-sm font-bold`} style={{height,width}}>{title}</button>
     )
 }
