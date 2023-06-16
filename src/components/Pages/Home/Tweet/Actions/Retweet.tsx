ffbc5e8ce043ee1d1c021a6de2eaa6439cf7b43a
antonm7 <migolkoanton@gmail.com>
2023-06-16 19:57:27 +0300

    styles changes for icons

--- src/components/Pages/Home/Tweet/Actions/Retweet.tsx
+++ src/components/Pages/Home/Tweet/Actions/Retweet.tsx
@@ -1,11 +1,11 @@
-import { RetweetIcon } from "../icons";
+import { RetweetIcon } from "@/components/common/Icons/Actions";
 import styles from './index.module.scss';
 
 export function Retweet() {
     return (
-        <div className={`flex items-center`}>
+        <div id={styles.retweet_container} className={`cursor-pointer flex items-center`}>
             <RetweetIcon id={styles.retweet}/>
-            <span className="sub_text text-sm pl-3">4</span>
+            <span className={`sub_text text-sm pl-3`}>4</span>
         </div>
     )
 }
--- src/components/Pages/Home/Tweet/Actions/Share.tsx
+++ src/components/Pages/Home/Tweet/Actions/Share.tsx
@@ -1,9 +1,9 @@
-import { ShareIcon } from "../icons";
+import { ShareIcon } from "@/components/common/Icons/Actions";
 import styles from './index.module.scss';
 
 export function Share() {
     return (
-        <div className={`flex items-center`}>
+        <div className={`cursor-pointer flex items-center`}>
             <ShareIcon id={styles.share}/>
         </div>
     )
