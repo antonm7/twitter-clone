2306f7a97709743e2dd5e9b987bfabe7d445762d
antonm7 <migolkoanton@gmail.com>
2023-06-16 19:54:52 +0300

    hover effect class

--- src/components/Menu/MoreSettings/SettingsWindow/index.tsx
+++ src/components/Menu/MoreSettings/SettingsWindow/index.tsx
@@ -14,7 +14,7 @@ type DropDownLineProps = {
 
 const DropDownLine = ({title,bottom}:DropDownLineProps):JSX.Element => {
     return (
-        <div id={bottom ? styles.bottom_hover_effect : ''} className={`p-4 flex items-center justify-between ${styles.hover_effect}`}>
+        <div id={bottom ? styles.bottom_hover_effect : ''} className={`p-4 flex items-center justify-between hover_effect hover_effect_transition`}>
             <span className='text-sm font-medium'>{title}</span>
             <Arrow />
         </div>
@@ -25,11 +25,11 @@ export default function SettingsWindow({active}:Props) {
     if(!active) return null
     return (
         <div className="cursor-pointer absolute bg-red-200 z-50 h-min w-96 left-0 rounded-xl border-2 border-white" id={styles.wrapper}>
-            <div className={`p-4 flex items-center ${styles.hover_effect}`} id={styles.top_hover_effect}>
+            <div className={`p-4 flex items-center hover_effect`} id={styles.top_hover_effect}>
                 <AtSign />
                 <h2 className='pl-8 text-xl font-medium'>Connect</h2>
             </div>
-            <div className={`p-4 flex items-center  ${styles.hover_effect}`}>
+            <div className={`p-4 flex items-center  hover_effect`}>
                 <Paint />
                 <h2 className='pl-8 text-xl font-medium'>Drafts</h2>
             </div>
