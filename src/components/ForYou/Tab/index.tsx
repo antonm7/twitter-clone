ef549ded6dcdfc09e0aa34ad763a1d53112d9b63
antonm7 <migolkoanton@gmail.com>
2023-06-16 19:53:42 +0300

    tab component changes

--- src/components/ForYou/Tab/index.tsx
+++ src/components/ForYou/Tab/index.tsx
@@ -1,12 +1,16 @@
-import { SettingsIcon } from "./SettingsIcon";
 import styles from '../index.module.scss';
+import { SettingsWithBackground } from "@/components/common/Icons/Settings";
 
-export default function Tab() {
+type Props = {
+    bottom?:boolean
+}
+
+export default function Tab({bottom}:Props) {
     return (
-        <div className="w-full h-min p-4" id={styles.tab_wrapper}>
+        <div className={`w-full h-min p-4 py-1 hover_effect hover_effect_transition ${bottom ? styles.bottom : ''}`}>
             <div className="flex justify-between items-center">
                 <span className="text-sm" id={styles.sub_title}>Sports - Trending</span>
-                <SettingsIcon />
+                <SettingsWithBackground />
             </div>
             <span className="font-medium block">Davies</span>
             <span className="text-sm" id={styles.sub_title}>13.8K Tweets</span>
