f1d0f7b5380d0321f194502985306d9f048e3bde
antonm7 <migolkoanton@gmail.com>
2023-06-16 19:53:53 +0300

    moved settings icon

--- src/components/ForYou/Tab/SettingsIcon.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-import { HiDotsHorizontal} from 'react-icons/hi/index'
-import styles from '../index.module.scss'
-
-export function SettingsIcon() {
-    return <HiDotsHorizontal className={styles.settings_icon}/>
-}
