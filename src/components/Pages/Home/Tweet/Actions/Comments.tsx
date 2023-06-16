176464fa0e70b33b66a3137c97201432ab2e699d
antonm7 <migolkoanton@gmail.com>
2023-06-16 19:56:55 +0300

    code clearence

--- src/components/Pages/Home/Tweet/Actions/Comments.tsx
+++ src/components/Pages/Home/Tweet/Actions/Comments.tsx
@@ -1,9 +1,9 @@
-import { Chat } from "../icons";
+import { Chat } from "@/components/common/Icons/Actions";
 import styles from './index.module.scss';
 
 export function Comments() {
     return (
-        <div className={`flex items-center`}>
+        <div id={styles.comments_container} className={`cursor-pointer flex items-center`}>
             <Chat id={styles.comment}/>
             <span className="sub_text text-sm pl-2">4</span>
         </div>
