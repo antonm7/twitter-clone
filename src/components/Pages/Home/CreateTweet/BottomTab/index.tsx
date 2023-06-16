ff1ebb0b7c7c235c79ca4cdf1d0492c99b4807d3
antonm7 <migolkoanton@gmail.com>
2023-06-16 19:55:54 +0300

    icons now have wrapper for background color

--- src/components/Pages/Home/CreateTweet/BottomTab/index.tsx
+++ src/components/Pages/Home/CreateTweet/BottomTab/index.tsx
@@ -1,16 +1,29 @@
 import StyledButton from "@/components/common/StyledButton";
 import {Calendar, Emoji, Gallery, Gif, Gps, List} from "./icons";
+import HoverAroundIcon from "@/components/common/HoverAroundIcon";
 
 export default function BottomTab() {
     return (
         <div className="flex justify-between items-center pt-4 pb-2">
             <div className="flex pl-2">
-                <Gallery />
-                <Gif />
-                <List />
-                <Emoji />
-                <Calendar />
-                <Gps />
+                <HoverAroundIcon className="mr-1 h-10 w-10">
+                    <Gallery />
+                </HoverAroundIcon>
+                <HoverAroundIcon className="mr-1 h-10 w-10">
+                    <Gif />
+                </HoverAroundIcon>
+                <HoverAroundIcon className="mr-1 h-10 w-10">
+                    <List />
+                </HoverAroundIcon>
+                <HoverAroundIcon className="mr-1 h-10 w-10">
+                    <Emoji />
+                </HoverAroundIcon>
+                <HoverAroundIcon className="mr-1 h-10 w-10">
+                    <Calendar />
+                </HoverAroundIcon>
+                <HoverAroundIcon className="mr-1 h-10 w-10">
+                    <Gps />
+                </HoverAroundIcon>
             </div>
             <StyledButton title={'Tweet'} height={'2.4rem'}/>
         </div>
