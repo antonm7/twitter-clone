d32805a64d5f5ea7f2e6b83c05e81626582ad918
antonm7 <migolkoanton@gmail.com>
2023-06-16 19:52:44 +0300

    all icons in one place

---
+++ src/components/common/Icons/Actions.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { FaChartBar, FaRegComment, FaRegHeart, FaShare } from 'react-icons/fa/index'
+import { FaRetweet } from 'react-icons/fa/index'
+
+type Props = {
+    id:string
+}
+
+export function Chat({id}:Props) {
+    return (
+        <div id={id} className='h-8 w-8 flex items-center justify-center rounded-full'>
+            <FaRegComment className='sub_text'/>
+        </div>
+    ) 
+}
+
+export function RetweetIcon({id}:Props) {
+    return (
+        <div id={id} className='h-8 w-8 flex items-center justify-center rounded-full'>
+            <FaRetweet className='sub_text'/>
+        </div>
+    ) 
+}
+
+export function HeartIcon({id}:Props) {
+    return (
+        <div id={id} className='h-8 w-8 flex items-center justify-center rounded-full'>
+            <FaRegHeart className='sub_text'/>
+        </div>
+    ) 
+}
+
+export function ChartIcon({id}:Props) {
+    return (
+        <div id={id} className='h-8 w-8 flex items-center justify-center rounded-full'>
+            <FaChartBar className='sub_text'/>
+        </div>
+    )
+}
+
+export function ShareIcon({id}:Props) {
+    return (
+        <div id={id} className='h-8 w-8 flex items-center justify-center rounded-full'>
+            <FaShare className='sub_text'/>
+        </div>
+    )
+}
+
+
+
+
