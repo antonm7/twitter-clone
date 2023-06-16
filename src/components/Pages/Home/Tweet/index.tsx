46c0162ce02ac0e92d023687e7b75c76d8c6a88c
antonm7 <migolkoanton@gmail.com>
2023-06-16 19:56:36 +0300

    change settings icon component

--- src/components/Pages/Home/Tweet/index.tsx
+++ src/components/Pages/Home/Tweet/index.tsx
@@ -1,10 +1,10 @@
 import { RegularProfileImageCircle } from "@/components/common/ProfileImageCircle";
-import { Settings } from "./icons";
 import { BottomBar } from "./BottomBar";
+import { SettingsWithBackground } from "@/components/common/Icons/Settings";
 
 export default function Tweet() {
     return (
-        <div className="flex border_bottom pt-2 pb-3 px-4">
+        <div className="hover_effect_light hover_effect_transition flex border_bottom pt-2 pb-3 px-4">
             <RegularProfileImageCircle />
             <div className="px-3">
                 <div className="flex justify-between items-center">
@@ -13,7 +13,7 @@ export default function Tweet() {
                         <span className="text-sm sub_text pr-1">@adamwathan</span>
                         <span className="text-sm sub_text">- 4m</span>
                     </div>
-                    <Settings />
+                    <SettingsWithBackground />
                 </div>
                 <div className="text-md block">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas obcaecati et quia.</p>
