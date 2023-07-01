f0d9a35a7fb7a0db74dd9cde5d13cfbe4b46d6d3
antonm7 <migolkoanton@gmail.com>
2023-07-01 15:34:23 +0300

    testing getting server session

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,12 @@
 import CreateTweet from "@/components/Pages/Home/CreateTweet";
 import Header from "@/components/Pages/Home/Header";
 import Tweet from "@/components/Pages/Home/Tweet";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 
 export default async function Home() {
+    const session = await getServerSession(authOptions)
+    console.log(session?.user)
     return (
         <>
             <Header />
