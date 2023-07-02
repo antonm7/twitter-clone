e88868e04884cfa312ed13a04f44d83895bb0dc8
antonm7 <migolkoanton@gmail.com>
2023-07-02 06:44:52 +0300

    access session user data

--- src/components/Menu/User/index.tsx
+++ src/components/Menu/User/index.tsx
@@ -1,15 +1,21 @@
-'use client'
+'use client';
 import { RegularProfileImageCircle } from "@/components/common/ProfileImageCircle";
 import styles from '../index.module.scss';
 import { SettingsIcon } from "@/components/common/Icons/Settings";
 import UserSettings from "./UserSettings";
 import { useEffect, useState } from "react";
 import { useHiddenLayerStore } from "@/store/HiddenLayer";
+import { useSession } from "next-auth/react";
 
-export default function User() {
-    const [active, setActive] = useState<boolean>(false)
+interface Props {
+    name:string
+    username:string
+}
 
+export default function User({name, username}:Props) {
     const hiddenLayerStore = useHiddenLayerStore(state => state)
+    const [active, setActive] = useState<boolean>(false)
+    const session = useSession()
 
     const turnOnActive = () => {
         setActive(true)
@@ -20,13 +26,14 @@ export default function User() {
         if(!hiddenLayerStore.visibility) setActive(false)
     },[hiddenLayerStore.visibility])
 
+
     return (
         <div onClick={() => turnOnActive()} className="flex w-full justify-between flex-nowrap mb-4 items-center p-4 py-2 rounded-3xl hover_effect hover_effect_transition ">
             <div className="flex">
                 <RegularProfileImageCircle />
                 <div className="pl-4">
-                    <span className="font-bold text-sm block">a_n_t_o_n</span>
-                    <span className={`block text-sm ${styles.sub_text}`}>@a_n_t_o_n_mig</span>
+                    <span className="font-bold text-sm block">{name}</span>
+                    <span className={`block text-sm ${styles.sub_text}`}>@{username}</span>
                 </div>
             </div>
             <SettingsIcon />
