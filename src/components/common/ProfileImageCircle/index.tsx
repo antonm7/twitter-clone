9dcd4375f3e195055b3a7dbf040df75a47193f52
antonm7 <migolkoanton@gmail.com>
2023-07-06 08:07:34 +0300

    profile image component

--- src/components/common/ProfileImageCircle/index.tsx
+++ src/components/common/ProfileImageCircle/index.tsx
@@ -2,6 +2,8 @@ import NoProfileImage from "./NoProfileImage";
 
 type Props = {
     onClick?:(e:any) => void
+    url?:string
+    size?:'sm' | 'lg'
 }
 
 export function RegularProfileImageCircle({onClick}:Props) {
@@ -10,4 +12,20 @@ export function RegularProfileImageCircle({onClick}:Props) {
             <NoProfileImage />
         </div>
     )
+}
+
+export function ProfileImage({onClick,url,size}:Props) {
+    if(!url) {
+        return (
+            <div className={`${size === 'lg' ? 'w-32 h-32' : 'w-10 h-10'} relative rounded-full`} onClick={onClick}>
+                <NoProfileImage size={size}/>
+            </div>
+        )
+    } else {
+        return (
+            <div className={`${size === 'lg' ? 'w-32 h-32' : 'w-10 h-10'} relative rounded-full`} onClick={onClick}>
+                <NoProfileImage size={size}/>
+            </div>
+        )
+    }
 }
