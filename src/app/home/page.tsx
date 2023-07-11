d374dc345a8d93ad1fdb60e0e99ea1b45a97be55
antonm7 <40800180+antonm7@users.noreply.github.com>
2023-07-11 11:49:18 +0300

    fetch tweets on home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,16 +2,63 @@ import CreateTweet from "@/components/Pages/Home/CreateTweet";
 import Header from "@/components/Pages/Home/Header";
 import Tweet from "@/components/Pages/Home/Tweet";
 import { authOptions } from "@/lib/auth";
+import { connectToDatabase } from "@/lib/mongodb";
+import { FullTweetData } from "@/lib/types/tweets";
+import { FullUserDocument } from "@/lib/types/user";
+import { ObjectId } from "mongodb";
 import { getServerSession } from "next-auth";
+
+async function get_tweets(userId:string):Promise<null | FullTweetData[]> {
+    try {
+        const db = await connectToDatabase()
+        
+        const user = await db.collection<FullUserDocument>('users').findOne({ _id: new ObjectId(userId) });
+        if(!user) throw new Error()
+
+        const following = user.following;
+        
+        const tweets = await db.collection('tweets').aggregate([
+            // query to match several conditions
+          { $match: { $or: [
+            // every tweet that has the userId field 
+            // as the current user, and all the users he follows.
+            { userId: { $in: [userId, ...following] } },
+            // every retweet that the user has made
+            { retweets: userId }
+          ] } },
+            //   sorting by creation to get a timeline correctly
+          { $sort: { createdAt: -1 } }
+        ]).toArray();
+        
+        return tweets as FullTweetData[];
+    } catch(e) {
+        console.log('error on fetching tweets')
+        return null
+    }
+} 
 
 export default async function Home() {
     const session = await getServerSession(authOptions)
-    console.log(session?.user)
+    const tweets_data = await get_tweets(session?.user._id!)
+
+    console.log('tahts the tweets data:', tweets_data)
     return (
         <>
             <Header />
             <CreateTweet />
-            <Tweet />
+            {tweets_data?.map(tweet => (
+                <Tweet 
+                    _id={tweet._id}
+                    text={tweet.text} 
+                    likes={tweet.likes} 
+                    dislikes={tweet.dislikes} 
+                    retweets={tweet.retweets} 
+                    userId={tweet.userId} 
+                    createdAt={tweet.createdAt} 
+                    user_name={tweet.user_name} 
+                    user_username={tweet.user_username} 
+                    user_img={tweet.user_img} 
+                />))}
         </>
     )
 }
