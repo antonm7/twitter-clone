818d8fd554a805cff336bba6b921a3766268c2b2
antonm7 <40800180+antonm7@users.noreply.github.com>
2023-07-11 11:49:27 +0300

    styles

--- src/components/common/StyledButton/index.tsx
+++ src/components/common/StyledButton/index.tsx
@@ -3,16 +3,23 @@ type Props = {
     height?:string
     width?:string,
     lgText?:boolean
+    onClick:() => void
 }
 
 export function StyledButtonBlue ({
     title,
     width,
     lgText,
-    height}:Props) {
+    height,
+    onClick}:Props) {
         let height_from_props = height || '2.4rem'
     return (
-        <button className={`bg-twitter-blue rounded-3xl px-5 w-min ${lgText ? 'text-lg' : 'text-sm'} text-sm font-bold`} style={{height:height_from_props,width}}>{title}</button>
+        <button 
+            onClick={onClick}
+            className={`bg-twitter-blue rounded-3xl px-5 w-min ${lgText ? 'text-lg' : 'text-sm'} text-sm font-bold`} 
+            style={{height:height_from_props,width}}>
+                {title}
+        </button>
     )
 }
 
@@ -20,9 +27,15 @@ export function StyledButtonWhite ({
     title,
     width,
     lgText,
-    height}:Props) {
+    height,
+    onClick}:Props) {
         let height_from_props = height || '2.4rem'
     return (
-        <button className={`bg-white rounded-3xl px-5 w-min ${lgText ? 'text-lg' : 'text-sm'} text-sm font-bold text-black`} style={{height:height_from_props,width}}>{title}</button>
+        <button 
+            onClick={onClick}
+            className={`bg-white rounded-3xl px-5 w-min ${lgText ? 'text-lg' : 'text-sm'} text-sm font-bold text-black`} 
+            style={{height:height_from_props,width}}>
+                {title}
+        </button>
     )
 }
