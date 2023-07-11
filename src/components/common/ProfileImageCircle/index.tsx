df3c5cde1cc71ecdaaaa833b38649cd35a5b0cad
antonm7 <40800180+antonm7@users.noreply.github.com>
2023-07-11 11:49:22 +0300

    styles

--- src/components/common/ProfileImageCircle/index.tsx
+++ src/components/common/ProfileImageCircle/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export function RegularProfileImageCircle({onClick}:Props) {
     return (
-        <div className="relative w-10 h-10 rounded-full" onClick={onClick}>
+        <div className="relative min-w-[2.5rem] w-10 h-10 rounded-full" onClick={onClick}>
             <NoProfileImage />
         </div>
     )
@@ -17,13 +17,13 @@ export function RegularProfileImageCircle({onClick}:Props) {
 export function ProfileImage({onClick,url,size}:Props) {
     if(!url) {
         return (
-            <div className={`${size === 'lg' ? 'w-32 h-32' : 'w-10 h-10'} relative rounded-full`} onClick={onClick}>
+            <div className={`${size === 'lg' ? 'min-w-[8rem] w-32 h-32' : 'min-w-[2.5rem] w-10 h-10'} relative rounded-full`} onClick={onClick}>
                 <NoProfileImage size={size}/>
             </div>
         )
     } else {
         return (
-            <div className={`${size === 'lg' ? 'w-32 h-32' : 'w-10 h-10'} relative rounded-full`} onClick={onClick}>
+            <div className={`${size === 'lg' ? 'min-w-[8rem] w-32 h-32' : 'min-w-[2.5rem] w-10 h-10'} relative rounded-full`} onClick={onClick}>
                 <NoProfileImage size={size}/>
             </div>
         )
