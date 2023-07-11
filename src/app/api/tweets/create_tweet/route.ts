78ad9ef1098e411aa44aa0d71d84d77044f68379
antonm7 <40800180+antonm7@users.noreply.github.com>
2023-07-11 11:48:44 +0300

    added zod and create tweet endpoint

--- src/app/api/tweets/create_tweet/route.ts
+++ src/app/api/tweets/create_tweet/route.ts
@@ -1,9 +1,60 @@
-import { type NextApiRequest, type NextApiResponse } from "next";
+import { connectToDatabase } from "@/lib/mongodb";
+import { InsertedTweet } from "@/lib/types/tweets";
+import { FullUserDocument } from "@/lib/types/user";
+import { ObjectId } from "mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+import { z } from 'zod';
 
-export async function Post(req:NextApiRequest,res:NextApiResponse) {
+
+export async function POST(req:Request,res:NextApiResponse) {
     try {
+        const body = await req.json()
+        console.log(body)
+        const schema  = z.object({
+            userId:z.string(),
+            text:z.string().max(124).min(1)
+        })
+
+        const validate_result = schema.safeParse(body)
+
+        if(!validate_result.success) {
+            return NextResponse.json({ error: 'Invalid Request' }, { status: 400 })
+        }
+
+        const db = await connectToDatabase()
+        
+        const {userId, text } = validate_result.data
+
+        const user = await db.collection<FullUserDocument>('users')
+        .findOne({_id: new ObjectId(userId)})
+
+        const tweet_object:InsertedTweet = {
+            text,
+            likes: [],
+            dislikes: [],
+            retweets: [],
+            userId,
+            user_username:user?.username!,
+            user_img:user?.profile_image!,
+            user_name:user?.name!,
+            createdAt:new Date()
+        }
+
+        const inserted_tweet = await db.collection('tweets').insertOne(tweet_object)
+
+        const updated_user = await db.collection<FullUserDocument>('users')
+        .updateOne({_id:new ObjectId(userId)},{$pull:{tweets:JSON.stringify(inserted_tweet.insertedId)}})
+
+        if(updated_user.acknowledged) {
+            return NextResponse.json({ok:true},{status:201})
+        } else {
+            throw new Error('User did not updated')
+        }
+
 
     } catch(e) {
-        
+        console.log('error on create_tweet Post request', e)
+        return NextResponse.error()
     }
 }
