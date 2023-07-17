a9f735f7bfa60d70fb7189bac27a4b8de29cf60a
antonm7 <migolkoanton@gmail.com>
2023-07-17 11:04:00 +0300

    added api endpoint

---
+++ src/app/api/users/user_window_data/route.ts
@@ -0,0 +1,27 @@
+import { url_parse } from "@/lib/helpers";
+import { connectToDatabase } from "@/lib/mongodb";
+import { type FullUserDocument } from "@/lib/types/user";
+import { ObjectId } from "mongodb";
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+
+export async function GET(req:NextApiRequest, res:NextApiResponse) {
+    try {
+        const parsed = url_parse(req.url as string)
+        const username = parsed.username
+
+        const db = await connectToDatabase();
+        const user = await db.collection<FullUserDocument>('users').findOne({username})
+        
+        return NextResponse.json({data: {
+            username:user?.username,
+            name:user?.name,
+            bio:user?.bio,
+            following:user?.following,
+            followers:user?.followers
+        }
+        })
+    } catch(e) {
+        return NextResponse.error()
+    }
+}
