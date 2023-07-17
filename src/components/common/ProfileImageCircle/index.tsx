f9307444ff988c1b2b11b4aaf4910d2f13acaa08
antonm7 <migolkoanton@gmail.com>
2023-07-17 11:05:43 +0300

    added user window component added

--- src/components/common/ProfileImageCircle/index.tsx
+++ src/components/common/ProfileImageCircle/index.tsx
@@ -1,8 +1,13 @@
+'use client';
+
+import { useState } from "react";
 import NoProfileImage from "./NoProfileImage";
+import UserWindow from "./UserWindow";
 
 type Props = {
     onClick?:(e:any) => void
     url?:string
+    username?:string
     size?:'sm' | 'lg'
 }
 
@@ -14,17 +19,25 @@ export function RegularProfileImageCircle({onClick}:Props) {
     )
 }
 
-export function ProfileImage({onClick,url,size}:Props) {
+export function ProfileImage({onClick,url,size,username}:Props) {
+    const [visible, setVisible] = useState<boolean>(false)
     if(!url) {
         return (
-            <div className={`${size === 'lg' ? 'min-w-[8rem] w-32 h-32' : 'min-w-[2.5rem] w-10 h-10'} relative rounded-full`} onClick={onClick}>
-                <NoProfileImage size={size}/>
+            <div className="relative">
+                <div onMouseEnter={() => setVisible(true)} 
+                    onMouseLeave={() => setVisible(false)} 
+                    className={`${size === 'lg' ? 'min-w-[5rem] w-20 h-20' : 'min-w-[2.5rem] w-10 h-10'} relative rounded-full`} onClick={onClick}>
+                    <NoProfileImage size={size}/>
+                </div>
+                {visible ? <UserWindow username={username ? username : ''}/> : null}
             </div>
         )
     } else {
         return (
-            <div className={`${size === 'lg' ? 'min-w-[8rem] w-32 h-32' : 'min-w-[2.5rem] w-10 h-10'} relative rounded-full`} onClick={onClick}>
-                <NoProfileImage size={size}/>
+            <div className="relative">
+                <div className={`${size === 'lg' ? 'min-w-[5rem] w-20 h-20' : 'min-w-[2.5rem] w-10 h-10'} relative rounded-full`} onClick={onClick}>
+                    <NoProfileImage size={size}/>
+                </div>
             </div>
         )
     }
