eba96efff40de81a1a2f0f189a66c4d629cee314
antonm7 <migolkoanton@gmail.com>
2023-07-23 16:27:58 +0300

    props added

--- src/components/common/ProfileImageCircle/index.tsx
+++ src/components/common/ProfileImageCircle/index.tsx
@@ -10,6 +10,7 @@ type Props = {
     onClick?:(e:any) => void
     url?:string
     username?:string
+    active_user_window:boolean
     size?:'sm' | 'lg'
 }
 
@@ -21,26 +22,28 @@ export function RegularProfileImageCircle({onClick}:Props) {
     )
 }
 
-export function ProfileImage({onClick,url,size,username}:Props) {
+export function ProfileImage({onClick,url,size,username,active_user_window}:Props) {
     const [visible, setVisible] = useState<boolean>(false)
     if(!url) {
         return (
-            <div className="relative">
                 <div onMouseEnter={() => setVisible(true)} 
                     onMouseLeave={() => setVisible(false)} 
                     className={`${size === 'lg' ? 'min-w-[5rem] w-20 h-20' : 'min-w-[2.5rem] w-10 h-10'} relative rounded-full`} onClick={onClick}>
                     <NoProfileImage size={size}/>
+                    {active_user_window && visible ? <UserWindow username={username ? username : ''}/> : null}
                 </div>
-                {visible ? <UserWindow username={username ? username : ''}/> : null}
-            </div>
         )
     } else {
         return (
-            <div className="relative">
-                <div className={`${size === 'lg' ? 'min-w-[5rem] w-20 h-20' : 'min-w-[2.5rem] w-10 h-10'} relative rounded-full`} onClick={onClick}>
+                <div 
+                    onMouseEnter={() => setVisible(true)} 
+                    onMouseLeave={() => setVisible(false)} className={`${size === 'lg' ?
+                     'min-w-[5rem] w-20 h-20' : 'min-w-[2.5rem] w-10 h-10'} relative rounded-full`} 
+                    onClick={onClick}
+                >
                     <YesProfileImage img={url}/>
+                    {active_user_window && visible ? <UserWindow username={username ? username : ''}/> : null}
                 </div>
-            </div>
         )
     }
 }
