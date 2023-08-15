900a4e0d1645247b91d5ce1837d5e4fb3fba5844
antonm7 <migolkoanton@gmail.com>
2023-08-15 11:54:49 +0300

    class names

--- src/components/Menu/User/index.tsx
+++ src/components/Menu/User/index.tsx
@@ -18,17 +18,17 @@ export default function User({name, username,img}:Props) {
 
     return (
         <TurnOnHiddenLayerWrapper 
-        className="flex w-full justify-between flex-nowrap mb-4 items-center p-4 py-2 rounded-3xl hover_effect hover_effect_transition "
+        className="relative flex w-full justify-between flex-nowrap mb-4 items-center p-4 py-2 rounded-3xl hover_effect hover_effect_transition "
             onActive={value => setActive(value)}>
             <div className="flex">
-                <ProfileImage url={img ? img : undefined}/>
+                <ProfileImage active_user_window={false} url={img ? img : undefined}/>
                 <div className="pl-4">
                     <span className="font-bold text-sm block">{name}</span>
                     <span className={`block text-sm ${styles.sub_text}`}>@{username}</span>
                 </div>
             </div>
             <SettingsIcon />
-            <OptionsWindow width="17rem" translateY="-65%" translateX="35%" active={active}>
+            <OptionsWindow width="17rem" translateY="-65%" translateX="0%" active={active}>
                 <Line title={"Add an existing account"}/>
                 <Line title={"Log out"} onClick={() => signOut()}/>
             </OptionsWindow>
