438e071091ef0b92b3b1a6443aefe3bd491279ed
antonm7 <migolkoanton@gmail.com>
2023-08-15 11:56:34 +0300

    wrapped hidden layer around the component

--- src/components/Pages/Home/Tweet/Actions/Comments.tsx
+++ src/components/Pages/Home/Tweet/Actions/Comments.tsx
@@ -1,11 +1,26 @@
+'use client';
+
 import { Chat } from "@/components/common/Icons/Actions";
 import styles from './index.module.scss';
+import { TurnOnHiddenLayerWrapper } from "@/components/common/TurnOnHiddenLayerWrapper";
+import { useState } from "react";
+import { AddComment } from "@/components/common/AddComment";
+import { TweetDataForClient } from "@/lib/types/tweets";
 
-export function Comments() {
+type Props = {
+    tweetData:TweetDataForClient
+}
+
+export function Comments({tweetData}:Props) {
+    const [active, setActive] = useState<boolean>(false)
+
     return (
-        <div id={styles.comments_container} className={`cursor-pointer flex items-center`}>
-            <Chat id={styles.comment}/>
-            <span className="sub_text text-sm pl-2">4</span>
-        </div>
+        <TurnOnHiddenLayerWrapper bg={true} onActive={(value) => setActive(value)}>
+            <div id={styles.comments_container} className={`cursor-pointer flex items-center`}>
+                <Chat id={styles.comment}/>
+                <span className="sub_text text-sm pl-2">4</span>
+            </div>
+            <AddComment tweetData={tweetData} active={active}/>
+        </TurnOnHiddenLayerWrapper>
     )
 }
