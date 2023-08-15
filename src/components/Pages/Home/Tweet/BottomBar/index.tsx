b45b360a60965e985e9f173dbde74601202acc3a
antonm7 <migolkoanton@gmail.com>
2023-08-15 11:56:42 +0300

    tweet data props added

--- src/components/Pages/Home/Tweet/BottomBar/index.tsx
+++ src/components/Pages/Home/Tweet/BottomBar/index.tsx
@@ -1,13 +1,19 @@
+import { TweetDataForClient } from "@/lib/types/tweets"
 import { Chart } from "../Actions/Chart"
 import { Comments } from "../Actions/Comments"
 import { Like } from "../Actions/Like"
 import { Retweet } from "../Actions/Retweet"
 import { Share } from "../Actions/Share"
+import { ObjectId } from "mongodb"
 
-export function BottomBar() {
+type Props = {
+    tweetData:TweetDataForClient
+}
+
+export function BottomBar({tweetData}:Props) {
     return (
         <div className="flex justify-between items-center pt-4 max-w-[80%]">
-            <Comments />
+            <Comments tweetData={tweetData}/>
             <Retweet />
             <Like />
             <Chart />
