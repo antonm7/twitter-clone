bf5b04c64c1272aea3a9902bf978d37d691287d7
antonm7 <migolkoanton@gmail.com>
2023-08-15 11:56:59 +0300

    close method added to store with types

--- src/store/HiddenLayer/store.ts
+++ src/store/HiddenLayer/store.ts
@@ -3,14 +3,20 @@ import {create} from 'zustand';
 type State = {
     visibility:boolean
     background:boolean
+    close:() => void
     changeVisibility: (value:boolean,bg?:boolean) => void
 }
 
 export const useHiddenLayerStore = create<State>(set => ({
     visibility:false,
     background:false,
-    changeVisibility: (value:boolean,bg?:boolean) => set({
+    changeVisibility: (value:boolean,bg?:boolean,closeable_value?:boolean) => set({
         visibility:value,
         background:bg || false
-    })
+    }),
+    close:() => set({
+            visibility:false,
+            background:false
+        }) 
+    
 }))
