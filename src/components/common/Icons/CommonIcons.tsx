7ed4c2fc676992c580049e4883890d43533bd2ed
antonm7 <migolkoanton@gmail.com>
2023-08-15 11:54:15 +0300

    code cleanup

--- src/components/common/Icons/CommonIcons.tsx
+++ src/components/common/Icons/CommonIcons.tsx
@@ -5,17 +5,18 @@ import { BsArrowLeft } from 'react-icons/bs/index'
 
 type Props = {
     id:string
+    method?:() => void
 }
 
-export function Xmark({id}:Props) {
+export function Xmark({id,method}:Props) {
     return (
         <div id={id} className='h-8 w-8 flex items-center justify-center rounded-full'>
-            <IoIosClose className='sub_text'/>
+            <IoIosClose onClick={method} className='sub_text'/>
         </div>
     ) 
 }
 
-export function BackIcon({id}:Props) {
+export function BackIcon({id,method}:Props) {
     return (
         <div id={id} className='h-8 w-8 flex items-center justify-center rounded-full'>
             <BsArrowLeft className='sub_text'/>
