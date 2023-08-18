1b61c515b9411f9d20e6218f0c44ec5ab55e8fad
antonm7 <migolkoanton@gmail.com>
2023-08-18 18:24:04 +0300

    wrapped link to navigate on tweet click

--- src/components/Pages/Home/Tweet/index.tsx
+++ src/components/Pages/Home/Tweet/index.tsx
@@ -2,6 +2,7 @@ import { ProfileImage, RegularProfileImageCircle } from "@/components/common/Pro
 import { BottomBar } from "./BottomBar";
 import { SettingsWithBackground } from "@/components/common/Icons/Settings";
 import { type FullTweetData } from "@/lib/types/tweets";
+import Link from "next/link";
 
 export default function Tweet({
     _id,
@@ -13,30 +14,34 @@ export default function Tweet({
     createdAt
 }:FullTweetData) {
     return (
-        <div className="hover_effect_light hover_effect_transition flex border_bottom pt-2 pb-3 px-4">
-            <ProfileImage active_user_window={true} username={user_username} url={user_img}/>
-            <div className="ml-4 w-full">
-                <div className="flex justify-between items-center">
-                    <div>
-                        <span className="text-base font-medium pr-1">{user_name}</span>
-                        <span className="text-sm sub_text pr-1">@{user_username}</span>
-                        <span className="text-sm sub_text">- 4m</span>
+        <Link href={`/tweet/${_id}`}>
+            <div className="hover_effect_light hover_effect_transition flex border_bottom pt-2 pb-3 px-4">
+                <ProfileImage active_user_window={true} username={user_username} url={user_img}/>
+                <div className="ml-4 w-full">
+                    <div className="flex justify-between items-center">
+                        <div>
+                            <span className="text-base font-medium pr-1">{user_name}</span>
+                            <span className="text-sm sub_text pr-1">@{user_username}</span>
+                            <span className="text-sm sub_text">- 4m</span>
+                        </div>
+                        <SettingsWithBackground />
                     </div>
-                    <SettingsWithBackground />
+                    <div className="text-md block">
+                        <p>{text}</p>
+                    </div>
+                    <BottomBar 
+                        size="sm"
+                        tweetData={{
+                        _id,
+                        userId,
+                        user_img,
+                        user_name,
+                        user_username,
+                        text,
+                        createdAt
+                    }}/>
                 </div>
-                <div className="text-md block">
-                    <p>{text}</p>
-                </div>
-                <BottomBar tweetData={{
-                     _id,
-                     userId,
-                     user_img,
-                     user_name,
-                     user_username,
-                     text,
-                     createdAt
-                }}/>
             </div>
-        </div>
+        </Link>
     )
 }
