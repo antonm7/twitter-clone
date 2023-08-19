c435a15f5e42f0f0b5c1d87d44833be3e0c2c8fa
antonm7 <migolkoanton@gmail.com>
2023-08-19 15:36:34 +0300

    avoid_wrapper added has helper function

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,4 +1,5 @@
 import qs, { type ParsedUrlQuery } from 'querystring'
+import type { RefObject } from 'react'
 
 export const url_parse = (url:string):ParsedUrlQuery => {
     const query =  url?.split('?')[1]
@@ -6,3 +7,13 @@ export const url_parse = (url:string):ParsedUrlQuery => {
 
     return parsed
 }
+
+export const avoid_wrapper  = (event:React.MouseEvent,componentRef:RefObject<HTMLDivElement>):boolean => {
+    if ( componentRef.current && event.target instanceof Node &&
+        !componentRef.current.contains(event.target)) {
+        // if the click happens inside the div element
+        return true
+    } else {
+        return false
+    }
+};
