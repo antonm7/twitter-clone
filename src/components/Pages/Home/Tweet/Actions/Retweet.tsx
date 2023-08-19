6ee4890c8ac67e8c7c598766709702a2a7177547
antonm7 <migolkoanton@gmail.com>
2023-08-19 15:36:02 +0300

    prevent default to cancel navigation

--- src/components/Pages/Home/Tweet/Actions/Retweet.tsx
+++ src/components/Pages/Home/Tweet/Actions/Retweet.tsx
@@ -1,6 +1,7 @@
 import { RetweetIcon } from "@/components/common/Icons/Actions";
 import styles from './index.module.scss';
 import { Sizes } from "@/lib/types/common";
+import React from "react";
 
 type Props = {
     size:Sizes
@@ -8,8 +9,12 @@ type Props = {
 }
 
 export function Retweet({size,activeNumberOfRetweets}:Props) {
+    const handle_retweet_action = async (e:React.MouseEvent) => {
+        e.preventDefault()
+    }
+
     return (
-        <div id={styles.retweet_container} className={`cursor-pointer flex items-center`}>
+        <div onClick={(e) => handle_retweet_action(e)} id={styles.retweet_container} className={`cursor-pointer flex items-center`}>
             <RetweetIcon id={styles.retweet} size={size}/>
             {activeNumberOfRetweets ? <span className={`sub_text text-sm pl-3`}>4</span> : null}
         </div>
