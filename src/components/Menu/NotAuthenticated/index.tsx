2672d7f9bbd1c361b0678e1877f10b2b6dee02d9
Anton Migolko <migolkoanton@gmail.com>
2023-08-21 16:10:35 +0300

    min width styling added

--- src/components/Menu/NotAuthenticated/index.tsx
+++ src/components/Menu/NotAuthenticated/index.tsx
@@ -4,7 +4,7 @@ import TwitterIcon from "../TwitterIcon";
 
 export default function NotAuthenticated() {
     return (
-        <div className="h-full w-full max-w-[650px] flex flex-col items-end">
+        <div className="h-full w-full min-w-[650px] max-w-[650px] flex flex-col items-end">
         <div className="h-full flex flex-col justify-between ml-8 mr-2">
          <div className="pr-12">
              <TwitterIcon />
