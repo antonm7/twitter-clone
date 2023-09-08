7536dc31105a16bf96fc2dfccff199100b1f4abd
antonm7 <migolkoanton@gmail.com>
2023-09-08 18:29:55 +0300

    recieving commentData and liked comments as props, checking if isUserLiked by checking array from props

--- src/components/common/FullComment/Index.tsx
+++ src/components/common/FullComment/Index.tsx
@@ -4,45 +4,50 @@ import { SettingsWithBackground } from "../Icons/Settings";
 import { BottomBar } from "@/components/Pages/Home/Tweet/BottomBar";
 import { ProfileImage } from "../ProfileImageCircle";
 
-export function FullComment(data:FullCommentData) {
+type Props = {
+    commentData:FullCommentData
+    likedComments:string[]
+}
+
+export function FullComment({commentData,likedComments}:Props) {
     return (
         <div>
             <div className="hover_effect_light hover_effect_transition flex border_bottom pt-2 pb-3 px-4">
                 <ProfileImage 
                     active_user_window={true} 
-                    username={data.user_username} 
-                    url={data.user_img}
+                    username={commentData.user_username} 
+                    url={commentData.user_img}
                 />
                 <div className="ml-4 w-full">
                     <div className="flex justify-between items-center">
                         <div>
-                            <span className="text-base font-medium pr-1">{data.user_name}</span>
-                            <span className="text-sm sub_text pr-1">@{data.user_username}</span>
+                            <span className="text-base font-medium pr-1">{commentData.user_name}</span>
+                            <span className="text-sm sub_text pr-1">@{commentData.user_username}</span>
                             <span className="text-sm sub_text">- 4m</span>
                         </div>
                         <SettingsWithBackground />
                     </div>
                     <div className="text-md block">
-                        <p>{data.text}</p>
+                        <p>{commentData.text}</p>
                     </div>
                     <BottomBar 
                         size="sm"
                         tweetData={{
-                            _id: data._id,
-                            userId: data.userId,
-                            user_img: data.user_img,
-                            user_name: data.user_name,
-                            user_username: data.user_username,
-                            text: data.text,
-                            createdAt: data.createdAt
+                            _id: commentData._id,
+                            userId: commentData.userId,
+                            user_img: commentData.user_img,
+                            user_name: commentData.user_name,
+                            user_username: commentData.user_username,
+                            text: commentData.text,
+                            createdAt: commentData.createdAt
                         }} 
-                        likes_length={data.likes}
-                        retweet_length={data.retweets} 
-                        comments_length={data.comments} 
-                        chart={data.views} 
-                        share={data.shares} 
-                        isUserLiked={false} 
-                        isUserRetweeted={false}/>
+                        likes_length={commentData.likes}
+                        retweet_length={commentData.retweets} 
+                        comments_length={commentData.comments} 
+                        chart={commentData.views} 
+                        share={commentData.shares} 
+                        isUserLiked={likedComments.includes(commentData._id.toString())} 
+                    />
                 </div>
             </div>
         </div>
