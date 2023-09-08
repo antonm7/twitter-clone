a630bcd3617620fb7227c6ea96fcdeadd50a0ac4
antonm7 <migolkoanton@gmail.com>
2023-09-08 18:32:47 +0300

    reciaving and loading liked_comments from server and setting state for it. passing userId to the load_comments request so I loaded the useSession hook there to get userId. Passing the data to the FullComment Component

--- src/components/Pages/Tweet/CommentsList.tsx
+++ src/components/Pages/Tweet/CommentsList.tsx
@@ -1,23 +1,29 @@
 'use client';
+
 import { FullComment } from "@/components/common/FullComment/Index";
 import { get_comments } from "@/lib/requests/tweet";
 import { FullCommentData } from "@/lib/types/tweets";
 import { ObjectId } from "mongodb";
+import { Session } from "next-auth/core/types";
+import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 
-const load_comments = async (parentTweet:string) => {
-    return await get_comments({parentTweet})
+const load_comments = async (parentTweet:string,userId?:string) => {
+    return await get_comments({parentTweet,userId})
 }
 
-export function CommentsList({parentTweet}:{parentTweet:string | ObjectId}) {
+export function CommentsList({parentTweet, userSession}:{parentTweet:string | ObjectId,userSession:Session | null}) {
+    const session = useSession()
     const [loading, setLoading] = useState<boolean>(true)
     const [comments,setComments] = useState<FullCommentData[]>([])
-    
+    const [likedComments, setLikedComments] = useState<string[]>([])
+
     useEffect(() => {
         setLoading(true)
         const load_comments_handler = async () => {
-            const body = await load_comments(parentTweet.toString())
-            setComments((body.data))
+            const body = await load_comments(parentTweet.toString(),userSession?.user._id.toString())
+            setComments(body.data.comments)
+            setLikedComments(body.data.liked_comments)
             setLoading(false)
         }
         load_comments_handler()
@@ -31,8 +37,10 @@ export function CommentsList({parentTweet}:{parentTweet:string | ObjectId}) {
         <div>
             {comments.map(
                 (comment:FullCommentData) => <FullComment 
-                    key={comment._id.toString()} 
-                    {...comment} />
+                        key={comment._id.toString()} 
+                        commentData={comment} 
+                        likedComments={likedComments}
+                    />
                 )}
         </div>
     )
